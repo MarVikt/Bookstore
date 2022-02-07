c5210bfc7a7c2bb0a77c97a121c007e33ae8ad92
MarVikt <v.m.podporogie@gmail.com>
2022-02-08 00:57:18 +0300

    header's adaptation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var menuButton = document.querySelector(".menu-mobile__button");
+  var menuButton = document.querySelector(".menu-mobile-button");
     menuButton.addEventListener('click', function() {
     // console.log("клик по кнопке мобильного меню");
     document.querySelector(".menu-mobile").classList.toggle('menu-mobile_visible');
