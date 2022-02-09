83a6bd40fe621e2b56de390ef30569853812ec45
MarVikt <v.m.podporogie@gmail.com>
2022-02-09 23:12:15 +0300

    added animation for recommended

--- js/main.js
+++ js/main.js
@@ -7,6 +7,37 @@ $(document).ready(function() {
   
   });
 
+  var choice01 = $('[data-toggle=f01]');
+  var choice02 = $('[data-toggle=f02]');
+  var choice03 = $('[data-toggle=f03]');
+  var choice04 = $('[data-toggle=f04]');
+  var choice05 = $('[data-toggle=f05]');
+  var choice06 = $('[data-toggle=f06]');
+  choice01.on('click', choiceFavourite01);
+  choice02.on('click', choiceFavourite02);
+  choice03.on('click', choiceFavourite03);
+  choice04.on('click', choiceFavourite04);
+  choice05.on('click', choiceFavourite05);
+  choice06.on('click', choiceFavourite06);
+  
+  function choiceFavourite01() {
+    document.querySelector('[data-toggle=f01]').classList.toggle('recommended__heart-favourite');
+  };
+  function choiceFavourite02() {
+    document.querySelector('[data-toggle=f02]').classList.toggle('recommended__heart-favourite');
+  };
+  function choiceFavourite03() {
+    document.querySelector('[data-toggle=f03]').classList.toggle('recommended__heart-favourite');
+  };
+  function choiceFavourite04() {
+    document.querySelector('[data-toggle=f04]').classList.toggle('recommended__heart-favourite');
+  };
+  function choiceFavourite05() {
+    document.querySelector('[data-toggle=f05]').classList.toggle('recommended__heart-favourite');
+  };
+  function choiceFavourite06() {
+    document.querySelector('[data-toggle=f06]').classList.toggle('recommended__heart-favourite');
+  };
 
 
 
