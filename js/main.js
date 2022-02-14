bb9c73de1967a192bab7558391aee43dd974cc76
MarVikt <v.m.podporogie@gmail.com>
2022-02-15 02:16:35 +0300

    created modal

--- js/main.js
+++ js/main.js
@@ -39,6 +39,45 @@ $(document).ready(function() {
     document.querySelector('[data-toggle=f06]').classList.toggle('recommended__heart-favourite');
   };
 
+  var closeModalButton = $(".modal__close");
+  var openModalButton = $('[data-toggle=modal-booking]');
+  // var modalButton = document.querySelector("data-toggle");
+  // console.log(openModalButton);
+  openModalButton.on('click', openModal);
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    // var modalBody = $("body");
+    modalOverlay.addClass('modal__overlay_visible');
+    modalDialog.addClass('modal__dialog_visible');
+    // modalBody.addClass('body_hidden');
+    document.body.style.overflow = "hidden";
+  };
+
+  closeModalButton.on('click', closeModal);
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    // var modalBody = $("body");
+    modalOverlay.removeClass('modal__overlay_visible');
+    modalDialog.removeClass('modal__dialog_visible');
+    // modalBody.removeClass('body_hidden');
+    document.body.style.overflow = "auto";
+  };
+
+  $(document).on('keydown',function(event) {
+    if (event.keyCode == 27) {
+      var modalOverlay = $(".modal__overlay");
+      var modalDialog = $(".modal__dialog");
+      // var modalBody = $("body");
+      modalOverlay.removeClass('modal__overlay_visible');
+      modalDialog.removeClass('modal__dialog_visible');
+      // modalBody.removeClass('body_hidden');
+      document.body.style.overflow = "auto";
+    }
+  });
+
 // валидация форм
 $(".registration__bar").validate({
   errorClass: "invalid",
@@ -50,6 +89,25 @@ $(".registration__bar").validate({
   }
 });
 
+$(".modal__send-form").validate({
+  errorClass: "invalid",
+  messages: {
+    modal_send_name: {
+      required: "Введите Ваше имя",
+      minlength: "Имя должно содержать хотя бы 2 буквы"
+    },
+    modal_send_phone: "Введите номер телефона +_(___)___-__-__",
+    modal_send_mail: {
+      required: "Введите адрес электронной почты",
+      email: "Адрес электронной почты должен быть в формате name@domain.com"
+    }
+  }
+});
+
+// номер телефона по маске
+$('.modal__send-phone').mask('+0(000)000-00-00', {
+  'translation': {0: {pattern: /[0-9]/}}
+});
 
 
 
